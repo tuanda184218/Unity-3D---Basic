dcd7c9e00cdff052299b2d54125088077b520bb8
tuan184218 <tuanpeterkill@gmail.com>
2022-04-06 03:45:23 +0700

    last day

---
+++ Flying bird/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+    private Vector3 direction;
+    public float gravity = -9.8f;
+    public float strength = 5f;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        if (Input.touchCount > 0) // ??
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * strength;
+            }
+        }
+
+        direction.y += gravity * Time.deltaTime; // van toc * thoi gian
+        transform.position += direction * Time.deltaTime; // vi tri
+    }
+
+    private void AnimateSprite() // thay doi chuyen dong cua con chim (vo canh)
+    {
+        spriteIndex++;
+        if (spriteIndex >= sprites.Length) // mang
+        {
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+        else if (other.gameObject.tag == "Scoring")
+        {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
+    }
+}
